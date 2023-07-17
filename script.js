3a4f0261008cc83a2d74f64839eb1b3e9fd0f808
Hunter Perry <skittles2519@hotmail.com>
2023-07-17 02:14:25 -0600

    5

--- script.js
+++ script.js
@@ -83,7 +83,9 @@ function getMealList(){
 
 // get recipe of the meal
 function getMealRecipe(e){    
-    let mealItem = e.target.parentElement.parentElement;
+    e.preventDefault();
+    if(e.target.classList.contains('recipe-btn')){
+        let mealItem = e.target.parentElement.parentElement;
     fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.dataset.id}`)
         .then(response => response.json())
         .then(data => mealRecipeModal(data.meals));
